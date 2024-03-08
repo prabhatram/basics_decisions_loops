231d21f6fc56ad02d1eb21225a290f2eb74af870
prabhatram <prabhat.ram@oulu.fi>
2024-03-08 12:12:23 +0800

    fixed tests

--- src/main/java/prog2/spring2024/exercise2/BookStore.java
+++ src/main/java/prog2/spring2024/exercise2/BookStore.java
@@ -1,13 +1,5 @@
 package prog2.spring2024.exercise2;
 
-/**
- * Hello world!
- *
- */
-public class BookStore 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class BookStore {
+    
 }
--- src/main/java/prog2/spring2024/exercise2/Main.java
+++ src/main/java/prog2/spring2024/exercise2/Main.java
@@ -1,5 +1,13 @@
 package prog2.spring2024.exercise2;
 
-public class Main {
-    
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
 }
